f4f669ca71c1790a76484a7ed28a31b06e185feb
jingg18 <102717592+jingg18@users.noreply.github.com>
2022-04-03 20:32:01 +0700

    Update Main.py
    I just update exercise 3

--- main.py
+++ main.py
@@ -5,10 +5,16 @@ def replace_last():
 # EXERCISE 2 by Son Sophak Otra
 def index_power(array, n):
     return array[n] ** 2 if n < len(array) else -1
+#EXERCISE 3 by Song Rithykun
+if b not in num_lst:
+        return num_lst
+    if len(num_lst) <= 0:
+        return num_lst
 
-# EXERCISE 3
-def remove_all_after():
-    pass
+    bindex = num_lst.index(b)
+    del num_lst[bindex+1:]
+
+    return num_lst
 
 # EXERCISE 4
 def chunking_by():
