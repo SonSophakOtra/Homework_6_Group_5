35130ff918ffe5ffc42c36b6a936c67ec6a2b4be
Otra <sonsophekotra12@gmail.com>
2022-04-03 20:13:36 +0700

    create main

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
-print ("git from csci")
+# EXERCISE 1
+def replace_last():
+    pass
 
-def say_hello(msg):
-    print("hello {msg}")
+# EXERCISE 2
+def index_power():
+    pass
+
+# EXERCISE 3
+def remove_all_after():
+    pass
+
+# EXERCISE 4
+def chunking_by():
+    pass
+
+# EXERCISE 5
+def backward_string_by_word():
+    pass
+
+def init():
+    pass
+
+if __name__ == "__main__":
+    init()
+    
